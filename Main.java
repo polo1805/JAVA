ac750c509a3be37475f901721cc51519d18fe3e3
polo1805 <131364737+polo1805@users.noreply.github.com>
2023-04-20 18:20:35 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidadFinanciera;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * 
+ * @author Alejandro
+ */
+public class Main {
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        CCuenta objetoCuenta;
+        double saldoActual;
+        objetoCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = objetoCuenta.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+        
+    
+    }
+  
+    
+    /**
+     * 
+     * @param c
+     * @param cantidad
+     * @param cantidadEsperada 
+     */
+    public static void probarIngresar(CCuenta c, int cantidad, int cantidadEsperada){
+        try {
+            c.ingresar(cantidad);
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    /**
+     * 
+     * @param c
+     * @param cantidad 
+     */
+    public static void probarRetirar(CCuenta c , int cantidad){
+        try {
+            c.retirar(cantidad);
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
